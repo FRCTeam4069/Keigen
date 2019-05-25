6fd82041a30819d6b999eec2d421e76adb1e9203
Redrield <redrield@gmail.com>
2019-05-25 14:31:07 -0400

    Add Vector typealias

--- src/main/kotlin/frc/team4069/keigen/vector.kt
+++ src/main/kotlin/frc/team4069/keigen/vector.kt
@@ -1,5 +1,7 @@
 package frc.team4069.keigen
 
+typealias Vector<D> = Matrix<D, `1`>
+
 operator fun <D: `100`> Matrix<D, `1`>.get(i: Int) = storage[i, 0]
 operator fun <D: `100`> Matrix<D, `1`>.set(i: Int, j: Double) {
     storage[i, 0] = j
