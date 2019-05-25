b3ed819d6b0c65b3100917a8c540ef53ab780304
Redrield <redrield@gmail.com>
2019-05-25 14:04:27 -0400

    Add more creator functions

--- src/main/kotlin/frc/team4069/keigen/creators.kt
+++ src/main/kotlin/frc/team4069/keigen/creators.kt
@@ -1,5 +1,6 @@
 package frc.team4069.keigen
 
+import org.ejml.dense.row.CommonOps_DDRM
 import org.ejml.simple.SimpleMatrix
 
 object mat {
@@ -40,4 +41,20 @@ object vec {
     }
 }
 
+fun <R: `100`, C: `100`> zeros(rows: Nat<R>, cols: Nat<C>) = Matrix(rows, cols, SimpleMatrix(rows.i, cols.i))
+fun <D: `100`> zeros(size: Nat<D>) = Vector(size, SimpleMatrix(size.i, 1))
+
+fun <D: `100`> eye(size: Nat<D>) = Matrix(size, size, SimpleMatrix.identity(size.i))
+
+fun <R: `100`, C: `100`> ones(rows: Nat<R>, cols: Nat<C>): Matrix<R, C> {
+    val out = SimpleMatrix(rows.i, cols.i)
+    CommonOps_DDRM.fill(out.ddrm, 1.0)
+    return Matrix(rows, cols, out)
+}
+fun <D: `100`> ones(size: Nat<D>): Vector<D> {
+    val out = SimpleMatrix(size.i, 1)
+    CommonOps_DDRM.fill(out.ddrm, 1.0)
+
+    return Vector(size, out)
+}
 
