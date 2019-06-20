a32365ace2f2ce41473a354880d840ee5f68ebf1
Redrield <redrield@gmail.com>
2019-06-20 16:43:12 -0400

    Replace number classes with `N{}`. Allow old name with typealias

--- src/main/kotlin/frc/team4069/keigen/Dim.kt
+++ src/main/kotlin/frc/team4069/keigen/Dim.kt
@@ -1,109 +1,211 @@
 package frc.team4069.keigen
 
-sealed class Num(open val i: Int)
+sealed class Num(val i: Int)
 
-interface Nat<T: Num> {
+interface Nat<T : Num> {
     val i: Int
 }
 
-open class `0`(override val i: Int = 0): Num(i) { companion object: `0`(), Nat<`0`> }
-open class `1`(override val i: Int = 1): Num(i) { companion object: `1`(), Nat<`1`> }
-open class `2`(override val i: Int = 2): Num(i) { companion object: `2`(), Nat<`2`> }
-open class `3`(override val i: Int = 3): Num(i) { companion object: `3`(), Nat<`3`> }
-open class `4`(override val i: Int = 4): Num(i) { companion object: `4`(), Nat<`4`> }
-open class `5`(override val i: Int = 5): Num(i) { companion object: `5`(), Nat<`5`> }
-open class `6`(override val i: Int = 6): Num(i) { companion object: `6`(), Nat<`6`> }
-open class `7`(override val i: Int = 7): Num(i) { companion object: `7`(), Nat<`7`> }
-open class `8`(override val i: Int = 8): Num(i) { companion object: `8`(), Nat<`8`> }
-open class `9`(override val i: Int = 9): Num(i) { companion object: `9`(), Nat<`9`> }
-open class `10`(override val i: Int = 10): Num(i) { companion object: `10`(), Nat<`10`> }
-open class `11`(override val i: Int = 11): Num(i) { companion object: `11`(), Nat<`11`> }
-open class `12`(override val i: Int = 12): Num(i) { companion object: `12`(), Nat<`12`> }
-open class `13`(override val i: Int = 13): Num(i) { companion object: `13`(), Nat<`13`> }
-open class `14`(override val i: Int = 14): Num(i) { companion object: `14`(), Nat<`14`> }
-open class `15`(override val i: Int = 15): Num(i) { companion object: `15`(), Nat<`15`> }
-open class `16`(override val i: Int = 16): Num(i) { companion object: `16`(), Nat<`16`> }
-open class `17`(override val i: Int = 17): Num(i) { companion object: `17`(), Nat<`17`> }
-open class `18`(override val i: Int = 18): Num(i) { companion object: `18`(), Nat<`18`> }
-open class `19`(override val i: Int = 19): Num(i) { companion object: `19`(), Nat<`19`> }
-open class `20`(override val i: Int = 20): Num(i) { companion object: `20`(), Nat<`20`> }
-open class `21`(override val i: Int = 21): Num(i) { companion object: `21`(), Nat<`21`> }
-open class `22`(override val i: Int = 22): Num(i) { companion object: `22`(), Nat<`22`> }
-open class `23`(override val i: Int = 23): Num(i) { companion object: `23`(), Nat<`23`> }
-open class `24`(override val i: Int = 24): Num(i) { companion object: `24`(), Nat<`24`> }
-open class `25`(override val i: Int = 25): Num(i) { companion object: `25`(), Nat<`25`> }
-open class `26`(override val i: Int = 26): Num(i) { companion object: `26`(), Nat<`26`> }
-open class `27`(override val i: Int = 27): Num(i) { companion object: `27`(), Nat<`27`> }
-open class `28`(override val i: Int = 28): Num(i) { companion object: `28`(), Nat<`28`> }
-open class `29`(override val i: Int = 29): Num(i) { companion object: `29`(), Nat<`29`> }
-open class `30`(override val i: Int = 30): Num(i) { companion object: `30`(), Nat<`30`> }
-open class `31`(override val i: Int = 31): Num(i) { companion object: `31`(), Nat<`31`> }
-open class `32`(override val i: Int = 32): Num(i) { companion object: `32`(), Nat<`32`> }
-open class `33`(override val i: Int = 33): Num(i) { companion object: `33`(), Nat<`33`> }
-open class `34`(override val i: Int = 34): Num(i) { companion object: `34`(), Nat<`34`> }
-open class `35`(override val i: Int = 35): Num(i) { companion object: `35`(), Nat<`35`> }
-open class `36`(override val i: Int = 36): Num(i) { companion object: `36`(), Nat<`36`> }
-open class `37`(override val i: Int = 37): Num(i) { companion object: `37`(), Nat<`37`> }
-open class `38`(override val i: Int = 38): Num(i) { companion object: `38`(), Nat<`38`> }
-open class `39`(override val i: Int = 39): Num(i) { companion object: `39`(), Nat<`39`> }
-open class `40`(override val i: Int = 40): Num(i) { companion object: `40`(), Nat<`40`> }
-open class `41`(override val i: Int = 41): Num(i) { companion object: `41`(), Nat<`41`> }
-open class `42`(override val i: Int = 42): Num(i) { companion object: `42`(), Nat<`42`> }
-open class `43`(override val i: Int = 43): Num(i) { companion object: `43`(), Nat<`43`> }
-open class `44`(override val i: Int = 44): Num(i) { companion object: `44`(), Nat<`44`> }
-open class `45`(override val i: Int = 45): Num(i) { companion object: `45`(), Nat<`45`> }
-open class `46`(override val i: Int = 46): Num(i) { companion object: `46`(), Nat<`46`> }
-open class `47`(override val i: Int = 47): Num(i) { companion object: `47`(), Nat<`47`> }
-open class `48`(override val i: Int = 48): Num(i) { companion object: `48`(), Nat<`48`> }
-open class `49`(override val i: Int = 49): Num(i) { companion object: `49`(), Nat<`49`> }
-open class `50`(override val i: Int = 50): Num(i) { companion object: `50`(), Nat<`50`> }
-open class `51`(override val i: Int = 51): Num(i) { companion object: `51`(), Nat<`51`> }
-open class `52`(override val i: Int = 52): Num(i) { companion object: `52`(), Nat<`52`> }
-open class `53`(override val i: Int = 53): Num(i) { companion object: `53`(), Nat<`53`> }
-open class `54`(override val i: Int = 54): Num(i) { companion object: `54`(), Nat<`54`> }
-open class `55`(override val i: Int = 55): Num(i) { companion object: `55`(), Nat<`55`> }
-open class `56`(override val i: Int = 56): Num(i) { companion object: `56`(), Nat<`56`> }
-open class `57`(override val i: Int = 57): Num(i) { companion object: `57`(), Nat<`57`> }
-open class `58`(override val i: Int = 58): Num(i) { companion object: `58`(), Nat<`58`> }
-open class `59`(override val i: Int = 59): Num(i) { companion object: `59`(), Nat<`59`> }
-open class `60`(override val i: Int = 60): Num(i) { companion object: `60`(), Nat<`60`> }
-open class `61`(override val i: Int = 61): Num(i) { companion object: `61`(), Nat<`61`> }
-open class `62`(override val i: Int = 62): Num(i) { companion object: `62`(), Nat<`62`> }
-open class `63`(override val i: Int = 63): Num(i) { companion object: `63`(), Nat<`63`> }
-open class `64`(override val i: Int = 64): Num(i) { companion object: `64`(), Nat<`64`> }
-open class `65`(override val i: Int = 65): Num(i) { companion object: `65`(), Nat<`65`> }
-open class `66`(override val i: Int = 66): Num(i) { companion object: `66`(), Nat<`66`> }
-open class `67`(override val i: Int = 67): Num(i) { companion object: `67`(), Nat<`67`> }
-open class `68`(override val i: Int = 68): Num(i) { companion object: `68`(), Nat<`68`> }
-open class `69`(override val i: Int = 69): Num(i) { companion object: `69`(), Nat<`69`> }
-open class `70`(override val i: Int = 70): Num(i) { companion object: `70`(), Nat<`70`> }
-open class `71`(override val i: Int = 71): Num(i) { companion object: `71`(), Nat<`71`> }
-open class `72`(override val i: Int = 72): Num(i) { companion object: `72`(), Nat<`72`> }
-open class `73`(override val i: Int = 73): Num(i) { companion object: `73`(), Nat<`73`> }
-open class `74`(override val i: Int = 74): Num(i) { companion object: `74`(), Nat<`74`> }
-open class `75`(override val i: Int = 75): Num(i) { companion object: `75`(), Nat<`75`> }
-open class `76`(override val i: Int = 76): Num(i) { companion object: `76`(), Nat<`76`> }
-open class `77`(override val i: Int = 77): Num(i) { companion object: `77`(), Nat<`77`> }
-open class `78`(override val i: Int = 78): Num(i) { companion object: `78`(), Nat<`78`> }
-open class `79`(override val i: Int = 79): Num(i) { companion object: `79`(), Nat<`79`> }
-open class `80`(override val i: Int = 80): Num(i) { companion object: `80`(), Nat<`80`> }
-open class `81`(override val i: Int = 81): Num(i) { companion object: `81`(), Nat<`81`> }
-open class `82`(override val i: Int = 82): Num(i) { companion object: `82`(), Nat<`82`> }
-open class `83`(override val i: Int = 83): Num(i) { companion object: `83`(), Nat<`83`> }
-open class `84`(override val i: Int = 84): Num(i) { companion object: `84`(), Nat<`84`> }
-open class `85`(override val i: Int = 85): Num(i) { companion object: `85`(), Nat<`85`> }
-open class `86`(override val i: Int = 86): Num(i) { companion object: `86`(), Nat<`86`> }
-open class `87`(override val i: Int = 87): Num(i) { companion object: `87`(), Nat<`87`> }
-open class `88`(override val i: Int = 88): Num(i) { companion object: `88`(), Nat<`88`> }
-open class `89`(override val i: Int = 89): Num(i) { companion object: `89`(), Nat<`89`> }
-open class `90`(override val i: Int = 90): Num(i) { companion object: `90`(), Nat<`90`> }
-open class `91`(override val i: Int = 91): Num(i) { companion object: `91`(), Nat<`91`> }
-open class `92`(override val i: Int = 92): Num(i) { companion object: `92`(), Nat<`92`> }
-open class `93`(override val i: Int = 93): Num(i) { companion object: `93`(), Nat<`93`> }
-open class `94`(override val i: Int = 94): Num(i) { companion object: `94`(), Nat<`94`> }
-open class `95`(override val i: Int = 95): Num(i) { companion object: `95`(), Nat<`95`> }
-open class `96`(override val i: Int = 96): Num(i) { companion object: `96`(), Nat<`96`> }
-open class `97`(override val i: Int = 97): Num(i) { companion object: `97`(), Nat<`97`> }
-open class `98`(override val i: Int = 98): Num(i) { companion object: `98`(), Nat<`98`> }
-open class `99`(override val i: Int = 99): Num(i) { companion object: `99`(), Nat<`99`> }
-open class `100`(override val i: Int = 100): Num(i) { companion object: `100`(), Nat<`100`> }
+object N0 : Num(0), Nat<N0>
+object N1 : Num(1), Nat<N1>
+object N2 : Num(2), Nat<N2>
+object N3 : Num(3), Nat<N3>
+object N4 : Num(4), Nat<N4>
+object N5 : Num(5), Nat<N5>
+object N6 : Num(6), Nat<N6>
+object N7 : Num(7), Nat<N7>
+object N8 : Num(8), Nat<N8>
+object N9 : Num(9), Nat<N9>
+object N10 : Num(10), Nat<N10>
+object N11 : Num(11), Nat<N11>
+object N12 : Num(12), Nat<N12>
+object N13 : Num(13), Nat<N13>
+object N14 : Num(14), Nat<N14>
+object N15 : Num(15), Nat<N15>
+object N16 : Num(16), Nat<N16>
+object N17 : Num(17), Nat<N17>
+object N18 : Num(18), Nat<N18>
+object N19 : Num(19), Nat<N19>
+object N20 : Num(20), Nat<N20>
+object N21 : Num(21), Nat<N21>
+object N22 : Num(22), Nat<N22>
+object N23 : Num(23), Nat<N23>
+object N24 : Num(24), Nat<N24>
+object N25 : Num(25), Nat<N25>
+object N26 : Num(26), Nat<N26>
+object N27 : Num(27), Nat<N27>
+object N28 : Num(28), Nat<N28>
+object N29 : Num(29), Nat<N29>
+object N30 : Num(30), Nat<N30>
+object N31 : Num(31), Nat<N31>
+object N32 : Num(32), Nat<N32>
+object N33 : Num(33), Nat<N33>
+object N34 : Num(34), Nat<N34>
+object N35 : Num(35), Nat<N35>
+object N36 : Num(36), Nat<N36>
+object N37 : Num(37), Nat<N37>
+object N38 : Num(38), Nat<N38>
+object N39 : Num(39), Nat<N39>
+object N40 : Num(40), Nat<N40>
+object N41 : Num(41), Nat<N41>
+object N42 : Num(42), Nat<N42>
+object N43 : Num(43), Nat<N43>
+object N44 : Num(44), Nat<N44>
+object N45 : Num(45), Nat<N45>
+object N46 : Num(46), Nat<N46>
+object N47 : Num(47), Nat<N47>
+object N48 : Num(48), Nat<N48>
+object N49 : Num(49), Nat<N49>
+object N50 : Num(50), Nat<N50>
+object N51 : Num(51), Nat<N51>
+object N52 : Num(52), Nat<N52>
+object N53 : Num(53), Nat<N53>
+object N54 : Num(54), Nat<N54>
+object N55 : Num(55), Nat<N55>
+object N56 : Num(56), Nat<N56>
+object N57 : Num(57), Nat<N57>
+object N58 : Num(58), Nat<N58>
+object N59 : Num(59), Nat<N59>
+object N60 : Num(60), Nat<N60>
+object N61 : Num(61), Nat<N61>
+object N62 : Num(62), Nat<N62>
+object N63 : Num(63), Nat<N63>
+object N64 : Num(64), Nat<N64>
+object N65 : Num(65), Nat<N65>
+object N66 : Num(66), Nat<N66>
+object N67 : Num(67), Nat<N67>
+object N68 : Num(68), Nat<N68>
+object N69 : Num(69), Nat<N69>
+object N70 : Num(70), Nat<N70>
+object N71 : Num(71), Nat<N71>
+object N72 : Num(72), Nat<N72>
+object N73 : Num(73), Nat<N73>
+object N74 : Num(74), Nat<N74>
+object N75 : Num(75), Nat<N75>
+object N76 : Num(76), Nat<N76>
+object N77 : Num(77), Nat<N77>
+object N78 : Num(78), Nat<N78>
+object N79 : Num(79), Nat<N79>
+object N80 : Num(80), Nat<N80>
+object N81 : Num(81), Nat<N81>
+object N82 : Num(82), Nat<N82>
+object N83 : Num(83), Nat<N83>
+object N84 : Num(84), Nat<N84>
+object N85 : Num(85), Nat<N85>
+object N86 : Num(86), Nat<N86>
+object N87 : Num(87), Nat<N87>
+object N88 : Num(88), Nat<N88>
+object N89 : Num(89), Nat<N89>
+object N90 : Num(90), Nat<N90>
+object N91 : Num(91), Nat<N91>
+object N92 : Num(92), Nat<N92>
+object N93 : Num(93), Nat<N93>
+object N94 : Num(94), Nat<N94>
+object N95 : Num(95), Nat<N95>
+object N96 : Num(96), Nat<N96>
+object N97 : Num(97), Nat<N97>
+object N98 : Num(98), Nat<N98>
+object N99 : Num(99), Nat<N99>
+object N100 : Num(100), Nat<N100>
+
+typealias `0` = N0
+typealias `1` = N1
+typealias `2` = N2
+typealias `3` = N3
+typealias `4` = N4
+typealias `5` = N5
+typealias `6` = N6
+typealias `7` = N7
+typealias `8` = N8
+typealias `9` = N9
+typealias `10` = N10
+typealias `11` = N11
+typealias `12` = N12
+typealias `13` = N13
+typealias `14` = N14
+typealias `15` = N15
+typealias `16` = N16
+typealias `17` = N17
+typealias `18` = N18
+typealias `19` = N19
+typealias `20` = N20
+typealias `21` = N21
+typealias `22` = N22
+typealias `23` = N23
+typealias `24` = N24
+typealias `25` = N25
+typealias `26` = N26
+typealias `27` = N27
+typealias `28` = N28
+typealias `29` = N29
+typealias `30` = N30
+typealias `31` = N31
+typealias `32` = N32
+typealias `33` = N33
+typealias `34` = N34
+typealias `35` = N35
+typealias `36` = N36
+typealias `37` = N37
+typealias `38` = N38
+typealias `39` = N39
+typealias `40` = N40
+typealias `41` = N41
+typealias `42` = N42
+typealias `43` = N43
+typealias `44` = N44
+typealias `45` = N45
+typealias `46` = N46
+typealias `47` = N47
+typealias `48` = N48
+typealias `49` = N49
+typealias `50` = N50
+typealias `51` = N51
+typealias `52` = N52
+typealias `53` = N53
+typealias `54` = N54
+typealias `55` = N55
+typealias `56` = N56
+typealias `57` = N57
+typealias `58` = N58
+typealias `59` = N59
+typealias `60` = N60
+typealias `61` = N61
+typealias `62` = N62
+typealias `63` = N63
+typealias `64` = N64
+typealias `65` = N65
+typealias `66` = N66
+typealias `67` = N67
+typealias `68` = N68
+typealias `69` = N69
+typealias `70` = N70
+typealias `71` = N71
+typealias `72` = N72
+typealias `73` = N73
+typealias `74` = N74
+typealias `75` = N75
+typealias `76` = N76
+typealias `77` = N77
+typealias `78` = N78
+typealias `79` = N79
+typealias `80` = N80
+typealias `81` = N81
+typealias `82` = N82
+typealias `83` = N83
+typealias `84` = N84
+typealias `85` = N85
+typealias `86` = N86
+typealias `87` = N87
+typealias `88` = N88
+typealias `89` = N89
+typealias `90` = N90
+typealias `91` = N91
+typealias `92` = N92
+typealias `93` = N93
+typealias `94` = N94
+typealias `95` = N95
+typealias `96` = N96
+typealias `97` = N97
+typealias `98` = N98
+typealias `99` = N99
+typealias `100` = N100
